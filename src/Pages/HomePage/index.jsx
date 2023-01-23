229133fc8b27a1aa6b4a53562e175e47e9e758d4
evgenii <gutenberg.eugen@gmail.com>
2023-01-23 19:50:13 +0100

    background replacement, textAnalysis added

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Canvas from '../../Components/Canvas'
 import TextAnalysis from '../../Components/TextAnalysis'
 
 export default function HomePage() {
   return (
     <div>
         <TextAnalysis />
+       
     </div>
   )
 }
